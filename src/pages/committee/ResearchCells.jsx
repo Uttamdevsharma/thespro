e30d18497706fdda5308ff61512c7761b257df93
Uttam <uttamrohit4545@gmail.com>
2025-07-08 23:53:12 +0600

    feat :implement cell members in committe role

--- src/pages/committee/ResearchCells.jsx
+++ src/pages/committee/ResearchCells.jsx
@@ -55,6 +55,7 @@ const ResearchCells = () => {
             onChange={(e) => setDescription(e.target.value)}
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
           />
+          
         </div>
         <button
           type="submit"
