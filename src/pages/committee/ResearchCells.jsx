c7b4d1602e9b711386cdbbcf2021902cb758b51f
Uttam <uttamrohit4545@gmail.com>
2025-07-31 21:58:35 +0600

    fix : color

--- src/pages/committee/ResearchCells.jsx
+++ src/pages/committee/ResearchCells.jsx
@@ -64,7 +64,7 @@ const ResearchCells = () => {
           />
           <button
             type="submit"
-            className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline col-span-full"
+            className="bg-blue-600 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline col-span-full"
           >
             Add Cell
           </button>
