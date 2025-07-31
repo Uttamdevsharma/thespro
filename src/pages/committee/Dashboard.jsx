120472ee3daa3df462e58cbb2638ec2cccb26ddd
Uttam <uttamrohit4545@gmail.com>
2025-07-31 21:58:09 +0600

    Fix : color

--- src/pages/committee/Dashboard.jsx
+++ src/pages/committee/Dashboard.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const Dashboard = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold text-gray-800">Welcome to the Committee Dashboard</h1>
+      <h1 className="text-3xl font-bold text-gray-700">Welcome to the Committee Dashboard</h1>
     </div>
   );
 };
